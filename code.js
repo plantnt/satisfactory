2aa39aee86bf7ddf714f88d41c26a30b21d773a6
plantnt <godzillagmk2204@gmail.com>
2023-09-16 15:50:11 -0500

    modified:   code.js 	new file:   img/arrw.png 	modified:   index.html 	modified:   style.css

---
+++ code.js
@@ -0,0 +1,25 @@
+const containerWelcome = document.querySelector('.container_welcome')
+const containerQuestions = document.querySelector('.container_questions')
+const startButton = document.querySelector('.start') 
+const progressBar = document.querySelector('.progress-bar-bg') 
+
+startButton.addEventListener('click', () => {
+    containerWelcome.style.transition = "opacity 0.5s, transform 0.5s"
+    containerWelcome.style.opacity = "0"
+    containerWelcome.style.transform = "translateY(-200px)"
+    setTimeout(() => {
+        containerWelcome.style.display = "none"
+        containerQuestions.style.display = "flex"
+        progressBar.style.display = "flex"
+    }, 500);
+})
+
+
+const selectText = document.getElementById("selector-text")
+const listItem = document.querySelectorAll(".options")
+
+listItem.forEach((item) => {
+    item.addEventListener('click', () => {
+        selectText.textContent = item.textContent
+    })
+})
